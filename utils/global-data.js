f15c7bb044fe0adb349289d894f08402451209a0
AntidoteDAO <105549035+AntidoteDAO@users.noreply.github.com>
2022-05-14 15:42:03 +0100

    changed the template plus added manifesto

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Antidote DAO';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Detoxifying workplaces for a better future';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Powered by web3';
 
   return {
     name,
