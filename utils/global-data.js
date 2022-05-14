c85e65ae967671988f821b473684113cd22de5bb
AntidoteDAO <105549035+AntidoteDAO@users.noreply.github.com>
2022-05-14 16:45:11 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Antidote DAO';
+    : 'Antidote Community';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Detoxifying workplaces for a better future';
